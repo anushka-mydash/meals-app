705c6b6dcfb0e60d9b99a008af1aa6bb9ad26613
Kumari Anushka <anushka@mydash.ai>
2022-09-16 00:20:23 +0530

    meals overview screen added

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,7 +1,7 @@
 import { View, Text, Pressable, StyleSheet, Platform } from 'react-native'
 import React from 'react'
 
-export default function CategoryGridTile({ title, color }) {
+export default function CategoryGridTile({ title, color, onPress }) {
   return (
     <View style={[styles.gridItem, { backgroundColor: color }]}>
       <Pressable
@@ -9,7 +9,9 @@ export default function CategoryGridTile({ title, color }) {
         style={({ pressed }) => [
           styles.button,
           pressed ? styles.buttonPress : null
-        ]}>
+        ]}
+        onPress={onPress}
+      >
         <View style={styles.innerContainer}>
           <Text style={styles.title}>{title}</Text>
         </View>
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,15 +1,23 @@
 import { FlatList, StyleSheet } from 'react-native';
+
 import CategoryGridTile from '../components/CategoryGridTile';
+
 import { CATEGORIES } from '../data/dummy-data'
 
-function renderCategoryItem(itemData) {
-  return <CategoryGridTile
-    title={itemData.item.title}
-    color={itemData.item.color}
-  />
-}
+export default function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
 
-export default function CategoriesScreen() {
+    function pressHandler() {
+      navigation.navigate("MealsOverview")
+    }
+
+    return <CategoryGridTile
+      title={itemData.item.title}
+      color={itemData.item.color}
+      onPress={pressHandler}
+    />
+  }
+
   return (
     <FlatList
       style={styles.screen}
