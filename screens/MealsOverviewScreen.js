bf38f03251a35b8c545cc198e00c97ae9d720f23
Kumari Anushka <anushka@mydash.ai>
2022-09-20 17:16:59 +0530

    confugured title for meals overview navigation

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,23 @@
 import { View, StyleSheet, FlatList } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 
-import { MEALS } from '../data/dummy-data'
+import { MEALS, CATEGORIES } from '../data/dummy-data'
 import MealItem from '../components/MealItem';
 
-export default function MealsOverviewScreen({ route }) {
+export default function MealsOverviewScreen({ route, navigation }) {
   const categoryId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter(
     mealItem => mealItem.categoryIds.indexOf(categoryId) >= 0)
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === categoryId).title
+
+    navigation.setOptions({
+      title: categoryTitle
+    })
+  }, [categoryId, navigation])
 
   function renderMealItem(itemData) {
     return (<MealItem {...itemData.item} />)
